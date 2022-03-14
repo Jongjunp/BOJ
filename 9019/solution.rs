aa3bb2510ac2b3b296af7f58e224b8d65b734fb1
박종준 <parkjongjun@bagjongjuns-MacBook-Air.local>
2022-03-15 02:00:49 +0900

    9019 timeout2

--- 9019/solution.rs
+++ 9019/solution.rs
@@ -11,46 +11,63 @@ fn main() {
         .filter(|s| !s.is_empty())        
         .map(|s| s.parse().unwrap())     
         .collect();
-
+        let mut ref_vec = vec![true;10000];
         let src:i32 = ab_vec[0];
         let dest:i32 = ab_vec[1];
         let mut walker:i32 = src;
-        let mut visit_nodes = VecDeque::new();
-        let mut way_nodes = VecDeque::from([String::from("")]);
-        while walker != dest {
-            let way = way_nodes.pop_front().unwrap();
+        let mut visit_nodes = VecDeque::from([(walker as u32, String::from(""))]);
+        loop {
+            let tuple = visit_nodes.pop_front().unwrap();
+            walker = tuple.0 as i32;
+            let way = tuple.1;
             let mut d_way = way.clone();
             let mut s_way = way.clone();
             let mut l_way = way.clone();
             let mut r_way = way.clone();
-            let d_trans = (walker*2)%10000;
-            let s_trans = (walker-1)%10000;
-            let l_trans = ((walker*10)%10000)+((walker*10)/10000);
-            let r_trans = (walker/10)+((walker%10)*1000);
-            if !(visit_nodes.contains(&d_trans)) {
-                visit_nodes.push_back(d_trans);
+            let d_trans = ((walker*2)%10000) as u32;
+            let s_trans = ((walker-1).rem_euclid(10000)) as u32;
+            let l_trans = (((walker*10)%10000)+((walker*10)/10000)) as u32;
+            let r_trans = ((walker/10)+((walker%10)*1000)) as u32;
+            if d_trans == dest as u32{
                 d_way.push_str("D");
-                way_nodes.push_back(d_way);
+                println!("{}",d_way);
+                break;
             }
-            if !(visit_nodes.contains(&s_trans)) {
-                visit_nodes.push_back(s_trans);
+            if s_trans == dest as u32 {
                 s_way.push_str("S");
-                way_nodes.push_back(s_way);
+                println!("{}",s_way);
+                break;
             }
-            if !(visit_nodes.contains(&l_trans)) {
-                visit_nodes.push_back(l_trans);
+            if l_trans == dest as u32 {
                 l_way.push_str("L");
-                way_nodes.push_back(l_way);
+                println!("{}",l_way);
+                break;
             }
-            if !(visit_nodes.contains(&r_trans)) {
-                visit_nodes.push_back(r_trans);
+            if r_trans == dest as u32 {
                 r_way.push_str("R");
-                way_nodes.push_back(r_way);
+                println!("{}",r_way);
+                break;
             }
-            walker = visit_nodes.pop_front().unwrap();
+            if ref_vec[d_trans as usize] {
+                d_way.push_str("D");
+                visit_nodes.push_back((d_trans,d_way));
+                ref_vec[d_trans as usize] = false;
+            }
+            if ref_vec[s_trans as usize] {
+                s_way.push_str("S");
+                visit_nodes.push_back((s_trans,s_way));
+                ref_vec[s_trans as usize] = false;
+            }
+            if ref_vec[l_trans as usize] {
+                l_way.push_str("L");
+                visit_nodes.push_back((l_trans,l_way));
+                ref_vec[l_trans as usize] = false;
+            }
+            if ref_vec[r_trans as usize] {
+                r_way.push_str("R");
+                visit_nodes.push_back((r_trans,r_way));
+                ref_vec[r_trans as usize] = false;
+            }
         }
-        let answer = way_nodes.pop_front().unwrap();
-        println!("{}", answer);
-        
     }
 }

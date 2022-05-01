13807be499aef5b3701d36881e0fee3b1ae5362a
박종준 <pjjkey@kaist.ac.kr>
2022-05-01 18:08:40 +0900

    10986 rust solved

--- 10986/solution.rs
+++ 10986/solution.rs
@@ -1,27 +1,26 @@
 use std::io::{self, BufRead};
 fn main() {
-    let mut N_M: Vec<u32> = io::stdin().lock().lines().next().unwrap().unwrap() 
+    let info: Vec<u32> = io::stdin().lock().lines().next().unwrap().unwrap() 
     .split(' ').map(|s| s.trim())    
     .filter(|s| !s.is_empty())        
     .map(|s| s.parse().unwrap())     
     .collect();
-    let mut M = N_M[1];
-    let mut seq: Vec<u32> = io::stdin().lock().lines().next().unwrap().unwrap() 
+    let m = info[1];
+    let seq: Vec<u32> = io::stdin().lock().lines().next().unwrap().unwrap() 
     .split(' ').map(|s| s.trim())    
     .filter(|s| !s.is_empty())        
-    .map(|s| s.parse().unwrap())     
+    .map(|s| s.parse().unwrap())
     .collect(); 
-    let mut pointer1 = 0;
-    let mut pointer2 = 0;
-    let mut sum = seq[pointer1];
-    let mut cnt = 0;
-    while (pointer1!=seq.len() && pointer2!=seq.len()) {
-        if (sum%M==0) {
-            cnt++;
-        }
-        else {
-
-        }
+    let mut ans : [i64;1000] = [0;1000];
+    let mut sum = 0;
+    for ele in seq.iter() {
+        sum += ele;
+        sum %= m;
+        ans[sum as usize] += 1;
     }
-
+    let mut answer = ans[0];
+    for num in ans {
+        answer += (num*(num-1))/2;
+    }
+    println!("{}",answer);
 }
---
+++ 1167/solution.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int V;
+    cin >> V;
+    for (int i=0; i<V; i++) {
+        
+    }
+}

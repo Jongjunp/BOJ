26554f5134f5957138461b1e731763edb14317be
Jongjunp <pjjkey@kaist.ac.kr>
2022-07-04 23:52:16 +0900

    1005 solved

--- 1005/solution.cpp
+++ 1005/solution.cpp
@@ -2,32 +2,77 @@
 #include <queue>
 #include <vector>
 using namespace std;
-int T,N,K,v1,v2,W;
-int build_time[1001] = {0};
-vector<int> dependency[1001];
-bool visited[1001] = {false};
-int topological_sort(int goal) {
-    queue<int> built;
-    for (int i=0; i<N; i++) {
+const int MAX = 1001;
 
-    }
-}
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	cin.tie(0); cout.tie(0);
+    int T,N,K,A,B;
     cin >> T;
-    for (int i=0; i<T; i++) {
+    for (int n=0; n<T; n++) {
+        vector<int> graph[MAX];
+        queue<int> order;
+        int degree[MAX] = {0};
+        int cost[MAX] = {0};
+        int dp[MAX];
+        for (int k=0; k<MAX; k++) {
+            dp[k] = -1;
+        }
+        int min_time = 0;
+        int targ_building;
+        bool find_targ = false;
         cin >> N >> K;
-        for (int j=1; j<=N; j++) {
-            cin >> build_time[j];
-            dependency[j].clear();
+        for (int i=1; i<=N; i++) {
+            cin >> cost[i];
         }
-        for (int k=0; k<K; k++) {
-            cin >> v1 >> v2;
-            dependency[v2].push_back(v1);
+        for (int i=0; i<K; i++) {
+            cin >> A >> B;
+            degree[B] += 1;
+            graph[A].push_back(B);
         }
-        cin >> W;
-        cout >> topological_sort(W) >> endl;
+        cin >> targ_building;
+        for (int i=1; i<=N; i++) {
+            if (degree[i]==0) {
+                order.push(i);
+                dp[i] = cost[i];
+                if (i==targ_building) {
+                    min_time = dp[i];
+                    find_targ = true;
+                    break;
+                }
+            }
+        }
+        if (find_targ) {
+            cout << min_time << endl;
+            continue;
+        }
+        for (int i=1; i<=N; i++) {
+            if (order.empty()) {
+                return 0;
+            }
+            int x = order.front();
+            order.pop();
+            for (int j=0; j<graph[x].size(); j++) {
+                int y = graph[x][j];
+                if (dp[y] < dp[x]) {
+                    dp[y] = dp[x];
+                }
+                if (--degree[y]==0) {
+                    order.push(y);
+                    dp[y] += cost[y];
+                    if (y==targ_building) {
+                        min_time = dp[y];
+                        find_targ = true;
+                    }
+                }
+                if (find_targ) {
+                    break;
+                }
+            }
+            if (find_targ) {
+                break;
+            }
+        }
+        cout << min_time << endl;
     }
 }

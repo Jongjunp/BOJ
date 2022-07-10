5a0a9f7de5b35b47f30651628fceaf2bb93f83e0
Jongjunp <pjjkey@kaist.ac.kr>
2022-07-10 16:37:03 +0900

    15480 solved

---
+++ 15480/solution.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+const int MAX_DEPTH = 18;
+const int MAX = 100001;
+int depth[MAX];
+vector<int> adj[MAX];
+int parent[MAX][MAX_DEPTH];
+void makeTree(int curr) {
+    vector<int>::iterator iter;
+    for (iter=adj[curr].begin(); iter!=adj[curr].end(); iter++) {
+        if (depth[*iter]==-1) {
+            parent[*iter][0] = curr;
+            depth[*iter] = depth[curr]+1;
+            makeTree(*iter);
+        }
+    }
+}
+int get_LCA(int a, int b) {
+    int u = a;
+    int v = b;
+    if (depth[u]<depth[v]) {
+        swap(u,v);
+    }
+    int diff = depth[u]-depth[v];
+    int p = 0;
+    while (diff) {
+        if (diff%2) {
+            u = parent[u][p];
+        }
+        p += 1;
+        diff /= 2;
+    }
+    if(u != v){
+        for(int j=MAX_DEPTH-1; j>=0; j--){
+            if(parent[u][j] != -1 && parent[u][j]!=parent[v][j]){
+                u = parent[u][j];
+                v = parent[v][j];
+            }
+        }
+        u = parent[u][0];
+    }
+    return u;
+}
+
+int main() {
+    memset(parent,-1,sizeof(parent));
+    memset(depth,-1,sizeof(depth));
+    depth[0] = 0;
+    int N,M,u,v,r;
+    cin >> N;
+    for (int i=0; i<N-1; i++) {
+        scanf("%d %d",&u,&v);
+        adj[u-1].push_back(v-1);
+        adj[v-1].push_back(u-1);
+    }
+    makeTree(0);
+    for (int i=0; i<MAX_DEPTH; i++) {
+        for (int j=1; j<N; j++) {
+            if (parent[j][i]!=-1) {
+                parent[j][i+1] = parent[parent[j][i]][i];
+            }
+        }
+    }
+    cin >> M;
+    for (int i=0; i<M; i++) {
+        scanf("%d %d %d",&u,&v,&r);
+        u--;
+        v--;
+        r--;
+        int uv_LCA = get_LCA(u,v);
+        int vr_LCA = get_LCA(v,r);
+        int ur_LCA = get_LCA(u,r);
+        int answer = uv_LCA;
+        if (depth[answer] < depth[vr_LCA]) {
+            answer = vr_LCA;
+        }
+        if (depth[answer] < depth[ur_LCA]) {
+            answer = ur_LCA;
+        }
+        printf("%d\n", answer+1);
+    }
+}

f04cd0b5a14c2340e1bd5d4da5a88dab7ee57c1d
Jongjunp <pjjkey@kaist.ac.kr>
2022-07-30 12:58:12 +0900

    3780 solved

--- 3780/solution.cpp
+++ 3780/solution.cpp
@@ -1,33 +1,48 @@
 #include <iostream>
-using namespace std;
+#include <algorithm>
 const int MAX = 20001;
+using namespace std;
+int center[MAX] = {0};
+int dist[MAX] = {0};
 
-int find_op(*int arr, int x) {
-
+int find_op(int x) {
+    if (x==center[x]) return x;
+    int center_x = find_op(center[x]);
+    dist[x] += dist[center[x]];
+    return center[x] = center_x;
 }
 
-void union_op(*int arr, int x, int y) {
-
+void union_op(int x, int y) {
+    dist[x] = abs(x-y)%1000;
+    center[x] = y;
 }
 
 int main() {
-    int T, N;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int T, N, i, j;
     char op;
-    scanf("%d", &T);
+    cin >> T;
     for (int t=0; t<T; t++) {
-        int center[MAX];
-        int size[MAX];
-        scanf("%d", &N);
+        cin >> N;
+        for (int u=1; u<=N; u++) {
+            center[u] = u;
+            dist[u] = 0;
+        }
         while (true) {
-            scanf("%c", &op);
-            if (c=='O') {
+            cin >> op;
+            if (op=='O') {
                 break;
             }
-            else if (c=='I') {
-
+            else if (op=='I') {
+                cin >> i >> j;
+                union_op(i,j);
             }
             else {
-
+                cin >> i;
+                int temp = find_op(i);
+                cout << dist[i] << "\n";
             }
         }
     }

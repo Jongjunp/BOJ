231e94a64c3b7c51720c217510b8462df0b33640
Jongjunp <pjjkey@kaist.ac.kr>
2022-08-11 02:15:40 +0900

    3665

--- 3665/solution.cpp
+++ 3665/solution.cpp
@@ -10,45 +10,67 @@ int main() {
     int T,N,M,a,b;
     cin >> T;
     for (int t=0; t<T; t++) {
-        vector<int> graph[MAX];
+        bool graph[MAX][MAX] = {false};
         queue<int> order;
         int degree[MAX] = {0};
         int seq[MAX] = {0};
+        int answer[MAX] = {0};
         cin >> N;
         for (int i=0; i<N; i++) {
             cin >> seq[i];
             degree[seq[i]] = i;
             for (int j=0; j<i; j++) {
-                graph[seq[i]].push_back(seq[j]);
+                graph[seq[i]][seq[j]] = true;
             }
         }
         cin >> M;
         for (int i=0; i<M; i++) {
             cin >> a >> b;
-            vector<int>::iterator iter;
-            bool isprecedence = true;
-            for (iter=graph[a].begin(); iter!=graph[a].end(); iter++) {
-                if (*iter==b) {
-                    graph[a].erase(iter);
-                    isprecedence = false;
-                    break;
-                }
-            }
-            if (!isprecedence) {
-                graph[b].push_back(a);
+            if (graph[a][b]) {
+                graph[a][b] = false;
+                degree[a] -= 1;
+                graph[b][a] = true;
+                degree[b] += 1;
             }
             else {
-                for (iter=graph[b].begin(); iter!=graph[b].end(); iter++) {
-                    if (*iter==a) {
-                        graph[b].erase(iter);
-                        break;
-                    }
-                }
-                graph[a].push_back(b);
+                graph[a][b] = true;
+                degree[a] += 1;
+                graph[b][a] = false;
+                degree[b] -= 1;
             }
         }
         for (int i=1; i<=N; i++) {
-            
+            if (degree[i]==0) {
+                order.push(i);
+                break;
+            }
+        }
+        bool abnormal = false;
+        for (int i=1; i<=N; i++) {
+            if (order.empty()) {
+                cout << "IMPOSSIBLE\n";
+                abnormal = true;
+                break;
+            }
+            else if (order.size()!=1) {
+                cout << "?\n";
+                abnormal = true;
+                break;
+            }
+            int x = order.front();
+            answer[i] = x;
+            order.pop();
+            for (int j=1; j<=N; j++) {
+                if (graph[j][x] && (--degree[j]==0)) {
+                    order.push(j);
+                }
+            }
+        }
+        if (!abnormal) {
+            for (int i=1; i<=N; i++) {
+                cout << answer[i] << " ";
+            }
+            cout << "\n";
         }
     }
 }
